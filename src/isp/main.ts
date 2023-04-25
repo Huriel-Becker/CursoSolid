47bfa7f6c1f381496cd2b0fe832785836492dfcb
Huriel Becker <becker.programador@outlook.com>
2023-04-25 15:11:56 -0300

    Implementação Interface Segregation Principle

--- src/isp/main.ts
+++ src/isp/main.ts
@@ -1,6 +1,11 @@
 /*
-Interface Segregation Principle -
-Os clientes não devem ser forçados a depender de types, interfaces ou membros abstratos que não utilizam
+Liskov substitution principle (Princípio da substituição de Liskov) -
+Se ϕ(x) é uma propriedade demonstrável dos objetos x de tipo T. Então ϕ(y)
+deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
+
+Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
+Mais simples ainda: Se meu programa espera um Animal, algo do tipo
+Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
 */
 import { ShoppingCart } from './classes/shopping-cart';
 import { Order } from './classes/order';
@@ -8,7 +13,6 @@ import { Messaging } from './services/messaging';
 import { Persistancy } from './services/persistancy';
 import { Product } from './classes/product';
 import { NoDiscount } from './classes/discount';
-import { EnterpriseCustomer /*IndividualCustomer*/ } from './classes/customer';
 
 // const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const tenPercentDiscount = new TenPercentDiscount();
@@ -16,21 +20,7 @@ const noDiscount = new NoDiscount();
 const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistancy = new Persistancy();
-// const individualCustomer = new IndividualCustomer(
-//   'Luiz',
-//   'Miranda',
-//   '111.111.111-11',
-// );
-const enterpriseCustomer = new EnterpriseCustomer(
-  'Empresa Gigante',
-  '111.111.111-11',
-);
-const order = new Order(
-  shoppingCart,
-  messaging,
-  persistancy,
-  enterpriseCustomer,
-);
+const order = new Order(shoppingCart, messaging, persistancy);
 
 shoppingCart.addItem(new Product('Camiseta', 49.9));
 shoppingCart.addItem(new Product('Caderno', 9.9));
